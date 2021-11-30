3639c21bf56375dd0da0a0cf3b0e8dbc15f1d2b8
lagshadow <fabrizio.ale@gmail.com>
2021-11-30 01:15:26 -0300

    agragado de la clase Ladron

---
+++ src/main/java/edu/fiuba/algo3/modelo/Ladron.java
@@ -0,0 +1,21 @@
+package edu.fiuba.algo3.modelo;
+
+public class Ladron {
+
+    public String nombre;
+    public String sexo;
+    public String hobby;
+    public String cabello;
+    public String senia;
+    public String vehiculo;
+
+    public Ladron(String nombre, String sexo, String hobby, String cabello, String senia, String vehiculo){
+        this.nombre = nombre;
+        this.sexo = sexo;
+        this.hobby = hobby;
+        this.cabello = cabello;
+        this.senia = senia;
+        this.vehiculo = vehiculo;
+    }
+}
+
