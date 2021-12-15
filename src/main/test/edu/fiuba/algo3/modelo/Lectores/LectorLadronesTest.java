138d642e9c39f8ac78179ecfe6cc0c420f144352
IVetrano <79949866+IVetrano@users.noreply.github.com>
2021-12-14 21:54:31 -0300

    Implementacion del lector de ciudades
    Implementacion del lector de ciudades a partir de un archivo JSON, se agrego tambien el metodo obtenerNombre en la clase ciudad para ser utilizado en pruebas y en la UI.

--- src/main/test/edu/fiuba/algo3/modelo/Lectores/LectorLadronesTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/Lectores/LectorLadronesTest.java
@@ -1,5 +1,6 @@
-package edu.fiuba.algo3.modelo;
+package edu.fiuba.algo3.modelo.Lectores;
 
+import edu.fiuba.algo3.modelo.Ladron;
 import edu.fiuba.algo3.modelo.lectoresDeArchivos.LectorLadrones;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
